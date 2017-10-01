42a6053229454775ae2df87681891153d90fba63
Zygo Blaxell <bees@furryterror.org>
2017-10-01 15:34:03 -0400

    roots: remove open_root_cache correctly
    BEESNOTE puts a message on the status message stack.  BEESINFO logs a
    message with rate limiting.  The message that was flooding the logs
    was coming from BEESINFO not BEESNOTE.
    
    Fix earlier commit which removed the wrong message.
    
    Signed-off-by: Zygo Blaxell <bees@furryterror.org>

--- src/bees-roots.cc
+++ src/bees-roots.cc
@@ -397,6 +397,7 @@ Fd
 BeesRoots::open_root_nocache(uint64_t rootid)
 {
 	BEESTRACE("open_root_nocache " << rootid);
+	BEESNOTE("open_root_nocache " << rootid);
 
 	// Stop recursion at the root of the filesystem tree
 	if (rootid == BTRFS_FS_TREE_OBJECTID) {
@@ -469,7 +470,7 @@ BeesRoots::open_root_nocache(uint64_t rootid)
 				THROW_CHECK2(runtime_error, new_root_id, rootid, new_root_id == rootid);
 				Stat st(rv);
 				THROW_CHECK1(runtime_error, st.st_ino, st.st_ino == BTRFS_FIRST_FREE_OBJECTID);
-				BEESINFO("open_root_nocache " << rootid << ": " << name_fd(rv));
+				// BEESINFO("open_root_nocache " << rootid << ": " << name_fd(rv));
 				return rv;
 			}
 		}
