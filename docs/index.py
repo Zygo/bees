6eb633b50400793e9f41a20e14bea4efc48c3c71
Calvin Walton <calvin.walton@kepstin.ca>
2021-12-23 00:24:02 -0500

    meson: Better handling of python scripts for source generation
    * Use the find_program function to find python rather than load the
      whole python module.
    * Adjust python scripts to write directly to an output file rather than
      requiring the meson wrapper for capture.
    * Move the python scripts to the custom_target command array rather than
      input files.
    
    Changes thanks to review by Eli Schwartz.

--- docs/index.py
+++ docs/index.py
@@ -1,5 +1,5 @@
 import sys
 
 # Rewrite relative links in index file to remove 'docs/' prefix
-with open(sys.argv[1]) as file:
-    sys.stdout.write(file.read().replace('docs/', ''))
+with open(sys.argv[1]) as input, open(sys.argv[2], 'w') as output:
+    output.write(input.read().replace('docs/', ''))
--- src/bees-usage.py
+++ src/bees-usage.py
@@ -2,8 +2,8 @@ import sys
 
 c_str_escape = str.maketrans({'\n': '\\n', '"': '\\"', '\\': '\\\\'})
 
-print("const char *BEES_USAGE = ")
-with open(sys.argv[1]) as file:
-    for line in file:
-        print('"{}"'.format(line.translate(c_str_escape)))
-print(";")
+with open(sys.argv[1]) as input, open(sys.argv[2], 'w') as output:
+    output.write('const char *BEES_USAGE = \n')
+    for line in input:
+        output.write('"{}"\n'.format(line.translate(c_str_escape)))
+    output.write(';\n')
